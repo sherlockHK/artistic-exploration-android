4909ef82cd294ed8248f0df3e0c560cdfcda7355
Kai <576717731@qq.com>
2017-05-27 14:40:46 +0800

    activity隐式启动， intent-filter: action, category, data{Uri("schema://host:port/path/pathPattern"), mimeType}

--- app/src/main/java/com/gakki/hk/artistic_exploration_android/fragment/ActivityFragment.java
+++ app/src/main/java/com/gakki/hk/artistic_exploration_android/fragment/ActivityFragment.java
@@ -1,11 +1,14 @@
 package com.gakki.hk.artistic_exploration_android.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gakki.hk.artistic_exploration_android.R;
 
@@ -15,8 +18,10 @@ import com.gakki.hk.artistic_exploration_android.R;
  * <p/>
  * 首页模块主界面
  */
-public class ActivityFragment extends Fragment {
+public class ActivityFragment extends Fragment implements View.OnClickListener {
 
+
+  private Button btn;
 
   public static ActivityFragment newInstance() {
 
@@ -27,5 +32,30 @@ public class ActivityFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_home, null);
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    btn = (Button) view.findViewById(R.id.btn_intent_filter);
+    btn.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    Intent intent = new Intent();
+
+    intent.setAction("artistic_exploration_android");
+    /**
+     * intent会自动加一个默认的categotry:android.intent.category.DEFAULT
+     * */
+    intent.addCategory("android.intent.category.DEFAULT_1");
+
+    /**
+     * data由URI和mimetype组成
+     * URI: scheme://host:port/path/pathPrefix/pathPattern
+     */
+    intent.setDataAndType(Uri.parse("http://abc"), "video/mpeg");
+
+    startActivity(intent);
   }
 }
