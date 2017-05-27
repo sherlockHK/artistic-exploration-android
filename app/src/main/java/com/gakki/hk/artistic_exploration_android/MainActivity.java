be8bfb534266c6e969e0dfd1d720fbd4864e4ea8
Kai <576717731@qq.com>
2017-05-27 11:30:30 +0800

    init directory

--- app/src/main/java/com/gakki/hk/artistic_exploration_android/MainActivity.java
+++ app/src/main/java/com/gakki/hk/artistic_exploration_android/MainActivity.java
@@ -1,10 +1,9 @@
 package com.gakki.hk.artistic_exploration_android;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +12,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.gakki.hk.artistic_exploration_android.fragment.ActivityFragment;
+import com.gakki.hk.artistic_exploration_android.fragment.IpcFragment;
+import com.gakki.hk.artistic_exploration_android.fragment.ViewTouchFragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -20,17 +23,13 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initFragments();
+    }
+
+    private void initView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -54,19 +53,14 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -74,28 +68,103 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private Fragment[] fragments;
+    private int currentTabIndex;
+
+    /**
+     * 初始化Fragments
+     */
+    private void initFragments() {
+
+        ActivityFragment activityFragment = ActivityFragment.newInstance();
+        IpcFragment ipcFragment = IpcFragment.newInstance();
+        ViewTouchFragment viewTouchFragment = ViewTouchFragment.newInstance();
+
+        fragments = new Fragment[]{
+                activityFragment,
+                ipcFragment,
+                viewTouchFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment,
+                activityFragment
+        };
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, activityFragment).show(activityFragment).commit();
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (item.getItemId()){
+            case R.id.nav_activity:
+                switchFragment(0);
+                break;
+            case R.id.nav_ipc:
+                switchFragment(1);
+                break;
+            case R.id.nav_view_touch:
+                switchFragment(2);
+                break;
+            case R.id.nav_view_principle:
+                switchFragment(3);
+                break;
+            case R.id.nav_remote_views:
+                switchFragment(4);
+                break;
+            case R.id.nav_drawable:
+                switchFragment(5);
+                break;
+            case R.id.nav_animation:
+                switchFragment(6);
+                break;
+            case R.id.nav_window:
+                switchFragment(7);
+                break;
+            case R.id.nav_four_component:
+                switchFragment(8);
+                break;
+            case R.id.nav_handler:
+                switchFragment(9);
+                break;
+            case R.id.nav_thread:
+                switchFragment(10);
+                break;
+            case R.id.nav_bitmap:
+                switchFragment(11);
+                break;
+            case R.id.nav_performance:
+                switchFragment(12);
+                break;
         }
 
+        item.setChecked(true);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /**
+     * Fragment切换
+     * @param index
+     */
+    private void switchFragment(int index) {
+        FragmentTransaction trx = getSupportFragmentManager().beginTransaction();
+        trx.hide(fragments[currentTabIndex]);
+
+        Fragment fragment = fragments[index];
+        if (!fragment.isAdded()) {
+            trx.add(R.id.container, fragment);
+        }
+        trx.show(fragment).commit();
+        currentTabIndex = index;
+    }
+
+
 }
---
+++ app/src/main/java/com/gakki/hk/artistic_exploration_android/fragment/ActivityFragment.java
@@ -0,0 +1,31 @@
+package com.gakki.hk.artistic_exploration_android.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gakki.hk.artistic_exploration_android.R;
+
+/**
+ * Created by hcc on 16/8/4 21:18
+ * 100332338@qq.com
+ * <p/>
+ * 首页模块主界面
+ */
+public class ActivityFragment extends Fragment {
+
+
+  public static ActivityFragment newInstance() {
+
+    return new ActivityFragment();
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_home, null);
+  }
+}
