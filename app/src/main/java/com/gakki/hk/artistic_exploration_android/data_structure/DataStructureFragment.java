30adc9218af07bb082775e37e2c577d5684bb1eb
Kai <576717731@qq.com>
2017-08-25 15:43:02 +0800

    data structure: linked list insert & delete

--- app/src/main/java/com/gakki/hk/artistic_exploration_android/data_structure/DataStructureFragment.java
+++ app/src/main/java/com/gakki/hk/artistic_exploration_android/data_structure/DataStructureFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.gakki.hk.artistic_exploration_android.R;
 
@@ -15,13 +17,59 @@ import com.gakki.hk.artistic_exploration_android.R;
 public class DataStructureFragment extends Fragment {
 
 
-  public static DataStructureFragment newInstance() {
-    return new DataStructureFragment();
-  }
+    private TextView console;
 
-  @Nullable
-  @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_data_structure, null);
-  }
+    public static DataStructureFragment newInstance() {
+        return new DataStructureFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_data_structure, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Button linked = (Button) view.findViewById(R.id.btn_linked_list);
+        Button stack_and_queue = (Button) view.findViewById(R.id.stack_and_queue);
+        Button string = (Button) view.findViewById(R.id.string);
+        Button tree = (Button) view.findViewById(R.id.tree);
+        Button graph = (Button) view.findViewById(R.id.graph);
+        Button btn_clear = (Button) view.findViewById(R.id.btn_clear);
+        console = (TextView) view.findViewById(R.id.tv_console);
+
+        linked.setOnClickListener(v -> linkedTest());
+        stack_and_queue.setOnClickListener(v -> linkedTest());
+        string.setOnClickListener(v -> linkedTest());
+        tree.setOnClickListener(v -> linkedTest());
+        graph.setOnClickListener(v -> linkedTest());
+        btn_clear.setOnClickListener(v -> clearLog());
+    }
+
+    private void linkedTest() {
+        LinkedList.Node node = LinkedList.generateNode(10);
+
+        //读取
+        LinkedList.Node first = LinkedList.get(node, 0);
+        LinkedList.Node last = LinkedList.get(node, 9);
+        log("linkedTest" + " first: " + first.data + " last: " + last.data);
+
+        //插入和删除
+        LinkedList.insert(node, 5, 101);
+        LinkedList.Node insertedNode = LinkedList.get(node, 5);
+        log("linkedTest" + " insert: " + insertedNode.data);
+
+        LinkedList.delete(node, 1);
+        LinkedList.Node deleted = LinkedList.get(node, 1);
+        log("linkedTest" + " first: " + deleted.data);
+    }
+
+    private void log(String text){
+        console.setText(console.getText() + "\n"+ text);
+    }
+
+    private void clearLog() {
+        console.setText("");
+    }
 }
---
+++ app/src/main/java/com/gakki/hk/artistic_exploration_android/data_structure/LinkedList.java
@@ -0,0 +1,87 @@
+package com.gakki.hk.artistic_exploration_android.data_structure;
+
+/**
+ * Created by HK on 2017/8/25.
+ * Email: kaihu1989@gmail.com.
+ */
+
+public class LinkedList {
+
+    /**
+     * 结点定义
+     */
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 生成链表数据
+     */
+    public static Node generateNode(int length) {
+        if (length <= 0) {
+            return null;
+        }
+        Node first = new Node(1, null);
+        //头插法
+        for (int i = 0; i < length - 1; i++) {
+            Node newNode = new Node(first.data + 1, null);
+            newNode.next = first;
+            first = newNode;
+        }
+        return first;
+    }
+
+    public static Node get(Node node, int index) {
+        if (node == null || index < 0) {
+            return null;
+        }
+
+        Node res = node;
+        for (int i = 0; i < index; i++) {
+            res = res.next;
+        }
+        return res;
+    }
+
+    public static boolean insert(Node node, int index, int data) {
+        if (node == null || index < 0) {
+            return false;
+        }
+
+        Node p = node;
+        for (int i = 0; i < index-1; i++) {
+            if (p == null) {
+                return false;
+            }
+            p = p.next;
+        }
+
+        p.next = new Node(data, p.next);
+        return true;
+    }
+
+    public static boolean delete(Node node, int index) {
+        if (node == null || index < 0) {
+            return false;
+        }
+
+        Node p = node;
+        for (int i = 0; i < index-1; i++) {
+            if (p == null){
+                return false;
+            }
+            p = p.next;
+        }
+
+        Node n = p.next;
+        p.next = n.next;
+        return true;
+    }
+
+}
